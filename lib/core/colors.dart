0438904b8f55855b2190c3739d445f6d2e83dc8a
DinhNam <dinhnamnghiem@gmail.com>
2025-08-05 14:37:45 +0700

    Setup app icon, splash, font

---
+++ lib/core/colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const kColorPrimary = Color(0xffB3E59F);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -44,6 +44,9 @@ class _MyHomePageState extends State<MyHomePage> {
           children: [
             const Text(
               'Hello',
+              style: TextStyle(
+                fontFamily: 'FiraMono',
+              ),
             ),
           ],
         ),
